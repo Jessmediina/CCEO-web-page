c53d8b9d2e7e1f714c3363e5bc9b5ebdbd9cb8bf
julioga1011 <julio.garcia@cceo.com.mx>
2023-11-25 09:46:44 -0600

    config: (eslint) 🔧 install eslint in the project

---
+++ .eslintrc.cjs
@@ -0,0 +1,60 @@
+module.exports = {
+    ignorePatterns: [],
+    env: {
+        node: true,
+    },
+    globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+    },
+    extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'plugin:vue/vue3-essential'],
+    rules: {
+        'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+        indent: 'off',
+        'vue/html-indent': [
+            'error',
+            'tab',
+            {
+                attribute: 1,
+                baseIndent: 1,
+                closeBracket: 0,
+            },
+        ],
+        'vue/html-closing-bracket-newline': [
+            'error',
+            {
+                singleline: 'never',
+                multiline: 'never',
+            },
+        ],
+        'vue/html-self-closing': [
+            'error',
+            {
+                html: {
+                    void: 'always',
+                    normal: 'always',
+                    component: 'always',
+                },
+                svg: 'always',
+                math: 'always',
+            },
+        ],
+        'vue/max-attributes-per-line': [
+            'error',
+            {
+                singleline: {
+                    max: 10,
+                },
+                multiline: {
+                    max: 10,
+                },
+            },
+        ],
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/attribute-hyphenation': 'off',
+        'vue/no-unused-vars': 'error',
+        'vue/multi-word-component-names': 'off',
+        'vue/no-reserved-component-names': 'off'
+    },
+};
