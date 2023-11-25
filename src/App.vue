73924a98641d61965f9d9958c63bd1d4d35f729c
julioga1011 <julio.garcia@cceo.com.mx>
2023-11-25 13:17:41 -0600

    lint (App.vue): :shirt: fix lint errors

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <h1 class="bg-sky-500 text-center text-3xl font-bold underline">
-        CCEO WEB PAGE
-    </h1>
+	<h1 class="bg-sky-500 text-center text-3xl font-bold underline">
+		CCEO WEB PAGE
+	</h1>
 </template>
 
 <script setup>
