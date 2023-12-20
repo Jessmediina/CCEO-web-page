409bedc21576f2ed55ed663cf4a9cebbd01f73f9
julioga1011 <julio.garcia@cceo.com.mx>
2023-12-20 13:32:19 -0600

    feat (NoticeOfPrivacy.vue): :sparkles: add page with notice of privacy

--- src/views/web-page/NoticeOfPrivacy.vue
+++ src/views/web-page/NoticeOfPrivacy.vue
@@ -1,3 +1,106 @@
 <template>
-	NoticeOfPrivacy
+	<div>
+		<div class="full-width bg-emerald-100">
+			<div class="text-center px-5 pt-[74px] pb-[90px]">
+				<h2 class="font-bold font-chivo text-[28px] leading-[32px] md:text-heading-2 mb-[22px]">
+					Aviso de privacidad
+				</h2>
+			</div>
+		</div>
+
+		<div class="px-[12px] md:px-[36px] mt-[70px] xl:px-0 mx-auto max-w-[1173px]">
+			<div class="grid text-gray-500 grid-cols-[1fr_55px] md:grid-cols-[168px_1fr_55px] text-[14px] leading-[25px] md:text-[18px] md:leading-[32px]">
+				<div class="content order-last col-span-2 md:ml-[78px] md:mr-[50px]">
+					<p class="mb-[30px]">
+						Para cualquier asunto relacionado con esta información puedes contactarte a
+						<a class="underline" href="mailto:contacto@cceo.com.mx">contacto@cceo.com.mx</a>
+					</p>
+					<p class="mb-[30px] text-justify">
+						<span class="font-bold">“CCEO-Software Development S.A.P.I.”</span> con domicilio en Av de los
+						Maestros #4623, Fracc Olinda, CP: 20200, Aguascalientes, México. Es responsable de recabar sus
+						datos personales, del uso que se le dé a los mismos y de su protección.
+					</p>
+					<ul class="ml-10 mb-[30px] list-decimal text-justify">
+						<li>
+							<span class="italic">FINALIDAD</span>.- Sus datos personales serán utilizados para fines
+							únicamente comerciales y de estadística. Para la finalidad antes mencionada, se le ha
+							requerido información de carácter personal no sensible tal como nombre, domicilio, teléfono
+							y toda aquella que no tenga la categoría de sensible.
+						</li>
+						<li>
+							<span class="italic">ACCESO A SUS DATOS PERSONALES</span>.- Usted tiene derecho de acceder a
+							sus datos personales que poseemos y a los detalles del tratamiento de los mismos, así como a
+							rectificarlos en caso de ser inexactos o incompletos; cancelarlos cuando considere que no se
+							requieren para alguna de las finalidades señaladas en el presente aviso de privacidad, estén
+							siendo utilizados para finalidades no consentidas o haya finalizado la relación contractual
+							o de servicio, o bien, oponerse al tratamiento de los mismos para fines específicos.
+						</li>
+						<li>
+							<span class="italic">SOLICITUD PARA EL ACCESO A SUS DATOS PERSONALES</span>.- Los mecanismos
+							que se han implementado para el ejercicio de dichos derechos son a través de la presentación
+							de la solicitud respectiva en la dirección Av de los Maestros #4623, Fracc Olinda, CP: 20200,
+							Aguascalientes, México, presentando directamente en recepción una solicitud que contenga los
+							siguientes datos:
+							<ul class="ml-10 list-disc">
+								<li>El nombre del titular y domicilio u otro medio para comunicarle la respuesta a su solicitud.</li>
+								<li>Indicar qué derecho se pretende ejercer. Si es acceso, rectificación, cancelación u oposición.</li>
+								<li>La descripción clara y precisa de los datos personales respecto de los que se busca ejercer alguno de los derechos.</li>
+							</ul>
+							<p class="my-[10px]">
+								Deberá mostrar identificación oficial válida (credencial de elector, licencia de conducir,
+								pasaporte) para acreditar su identidad como titular. En caso de que el trámite lo realice
+								algún representante deberá exhibir carta poder firmada ante 2 testigos con copia de la
+								identificación oficial del titular de los datos.
+							</p>
+						</li>
+						<li>
+							<span class="italic">PLAZO MÁXIMO DE RESPUESTA A LA SOLICITUD</span>.- El plazo máximo de
+							respuesta a las solicitudes será de veinte días naturales contados desde la fecha en que se
+							recibió la solicitud de acceso, rectificación, cancelación u oposición. Dicho plazo podrá
+							ser ampliado una sola vez por un periodo igual, siempre y cuando así lo justifiquen las
+							circunstancias del caso.
+						</li>
+						<li>
+							<span class="italic">REVOCACIÓN DE CONSENTIMIENTO</span>.- Así mismo, en todo momento, usted
+							podrá revocar el consentimiento que nos ha otorgado para el tratamiento de sus datos personales,
+							a fin de que dejemos de hacer uso de los mismos. Para ello, es necesario que presente petición
+							y documentos para acreditar personalidad, en los mismos términos que se establecen para el
+							ejercicio de derechos de acceso, rectificación, cancelación u oposición. En un plazo máximo de
+							15 días naturales atenderemos su petición y le informaremos sobre la procedencia de la misma a
+							través de llamada telefónica y/o correo electrónico, para que proceda a recibir su informe
+							respectivo a nuestras instalaciones. Cabe mencionar que tanto el ejercicio de un derecho
+							ARCO y la revocación de consentimiento no tiene costo alguno, más que el que se derive de la
+							reproducción de la información.
+						</li>
+						<li>
+							<span class="italic">PRIVACIDAD DE SUS DATOS PERSONALES</span>.- Asimismo, le informamos que
+							sus datos personales no serán transferidos ni tratados por personas distintas a esta empresa.
+							En caso de ser necesaria la transmisión, en virtud de que transferiremos datos personales se
+							le notificará su transferencia y los motivos para los que fue realizada, en caso que no
+							manifieste desacuerdo ante esto, se tomará como una aceptación tácita.
+						</li>
+						<li>
+							<span class="italic">MODIFICACIÓN DE EL AVISO DE PRIVACIDAD</span>.- Nos reservamos el
+							derecho de efectuar en cualquier momento modificaciones o actualizaciones al presente aviso
+							de privacidad, para la atención de novedades legislativas, políticas internas o nuevos
+							requerimientos para la prestación u ofrecimiento de nuestros servicios o productos. Estas
+							modificaciones estarán disponibles al público a través de los siguientes medios: anuncios
+							visibles y trípticos o folletos disponibles en nuestro establecimiento.
+						</li>
+						<li>
+							<span class="italic">AGRAVIO AL DERECHO DE PROTECCIÓN DE DATOS PERSONALES</span>.- Si usted
+							considera que su derecho de protección de datos personales ha sido lesionado por alguna
+							conducta de nuestros empleados o de nuestras actuaciones o respuestas, presume que en el
+							tratamiento de sus datos personales existe alguna violación a las disposiciones previstas
+							en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares, podrá
+							interponer la queja o denuncia correspondiente ante el IFAI, para mayor información visite
+							www.ifai.org.mx.
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
+<script setup>
+</script>
