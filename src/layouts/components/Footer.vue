ee579f097244bde2ed1e1bd503894617a64873af
Ximena <jessica.medina@cceo.com.mx>
2025-04-16 12:11:17 -0600

    New feature :sparkles: page organization

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-	<footer class="mt-[92px] lg:mt-[150px] xl:mt-[200px] mb-[30px]">
+	<footer class="full-width banner-hero banner-1 mt-[92px] lg:mt-[150px] xl:mt-[200px] mb-[30px] bg-sky-900">
 		<div class="px-[12px] md:px-[36px] mt-[70px] xl:px-0">
-			<div class="flex flex-col items-center gap-2 mb-14 md:flex-row md:justify-between">
+			<div class="flex flex-col items-center gap-2 mb-14 md:flex-row md:justify-between m-20">
 				<img class="h-full w-full object-cover max-w-[162px]" src="@/assets/images/logo.png" alt="Logotipo de CCEO - Software Development" />
 				<div class="flex items-center flex-col gap-5 md:flex-row lg:gap-[30px]">
-					<p class="text-heading-6 font-chivo font-bold">¿Listo para empezar?</p>
+					<p class="text-white text-heading-6 font-chivo font-bold">¿Listo para empezar?</p>
 					<button type="button">
-						<a class="flex items-center inline-block z-10 relative transition-all duration-200 group px-[22px] py-[15px] lg:px-[32px] lg:py-[22px] rounded-md bg-gray-900 text-white hover:bg-gray-100 hover:text-gray-900 hover:-translate-y-[2px] text-white bg-gray-900 w-fit" href="#">
+						<a class="flex items-center z-10 relative transition-all duration-200 group px-[22px] py-[15px] lg:px-[32px] lg:py-[22px] rounded-md bg-gray-900 text-white hover:bg-gray-100 hover:text-gray-900 hover:-translate-y-[2px] text-white bg-gray-900 w-fit" href="#">
 							<span class="block text-inherit w-full h-full rounded-md text-lg font-chivo font-semibold">
 								Crear una cuenta
 							</span>
@@ -14,10 +14,10 @@
 					</button>
 				</div>
 			</div>
-			<div class="w-full bg-gray-200 h-[1px] mb-[52px]" />
-			<div class="text-gray-600 grid gird-cols-1 gap-8 mb-[48px] md:grid-cols-2 lg:grid-cols-5 xl:gap-[98px]">
+			<div class="w-full bg-white h-[1px] mb-[52px]" />
+			<div class="text-white grid gird-cols-1 gap-8 mb-[48px] md:grid-cols-2 lg:grid-cols-5 xl:gap-[98px] m-20">
 				<div class="col-span-2">
-					<h5 class="text-heading-5 font-chivo font-bold text-gray-900 mb-5 text-[18px]">Contacto</h5>
+					<h5 class="text-heading-5 font-chivo font-bold text-white mb-5 text-[18px]">Contacto</h5>
 					<p class="text-text mb-5">
 						Av. de los Maestros #4623, Fracc. Olinda, CP: 20200, Aguascalientes, México
 					</p>
@@ -25,106 +25,106 @@
 					<a class="text-text underline" href="mailto:contacto@cceo.com.mx">contacto@cceo.com.mx</a>
 				</div>
 				<div>
-					<h5 class="text-heading-5 font-chivo font-bold text-gray-900 mb-5 text-[18px]">Nosotros</h5>
+					<h5 class="text-heading-5 font-chivo font-bold text-white mb-5 text-[18px]">Nosotros</h5>
 					<ul>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'index' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Inicio</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'about' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Nosotros</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'services' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Servicios</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'products' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Productos</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'industries' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Industrias</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'contact' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Contacto</span>
 							</router-link>
 						</li>
 					</ul>
 				</div>
 				<div>
-					<h5 class="text-heading-5 font-chivo font-bold text-gray-900 mb-5 text-[18px]">Enlaces importantes</h5>
+					<h5 class="text-heading-5 font-chivo font-bold text-white mb-5 text-[18px]">Enlaces importantes</h5>
 					<ul>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'successStories' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Casos de Éxito</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'careers' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Carreras</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'termsAndConditions' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Terminos y Condiciones</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'noticeOfPrivacy' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Aviso de privacidad</span>
 							</router-link>
 						</li>
 					</ul>
 				</div>
 				<div>
-					<h5 class="text-heading-5 font-chivo font-bold text-gray-900 mb-5 text-[18px]">Soporte</h5>
+					<h5 class="text-heading-5 font-chivo font-bold text-white mb-5 text-[18px]">Soporte</h5>
 					<ul>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'customerSupport' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Soporte a clientes</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'contact' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Contacto</span>
 							</router-link>
 						</li>
 						<li class="mb-2">
 							<router-link
 								:to="{ name: 'frequentQuestions' }"
-								class="transition-all duration-200 hover:text-green-900 hover:pl-[3px]">
+								class="transition-all duration-200 hover:text-primary-0 hover:pl-[3px]">
 								<span>Preguntas Frecuentes</span>
 							</router-link>
 						</li>
@@ -132,7 +132,7 @@
 				</div>
 			</div>
 			<div class="w-full bg-gray-200 h-[1px] mb-[46px]" />
-			<div class="text-gray-400 lg:flex lg:items-center lg:justify-between">
+			<div class="text-gray-400 lg:flex lg:items-center lg:justify-between m-20">
 				<div class="md:flex md:items-center md:gap-6">
 					<p class="text-lead font-bold">CCEO - Software Development 2023</p>
 				</div>
--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -1,57 +1,72 @@
 <template>
-	<header class="px-[12px] md:px-[36px] h-auto full-width relative py-[15px] first-letter:lg:py-[26px]">
-		<div class="mt-[70px] xl:px-0 flex items-center justify-between mx-auto relative !mt-0 max-w-[1320px]">
+	<header :class="isScrolled ? 'bg-white fixed top-0 left-0 w-full shadow-md z-50' : 'relative'">
+		<div class="xl:px-6 flex items-center justify-between mx-auto max-w-full w-full relative !mt-0">
 			<router-link
 				:to="{ name: 'index' }"
 				class="flex">
-				<img class="logo z-50 w-[90px] md:w-[101px] lg:w-[122px] xl:w-[138px]" src="@/assets/images/logo.png" alt="Logotipo de CCEO - Software Development" />
+				<img class="m-4 8logo z-50 w-[90px] md:w-[101px] lg:w-[122px] xl:w-[120px]" src="@/assets/images/logo.png" alt="Logotipo de CCEO - Software Development" />
 			</router-link>
 			<nav class="z-50 absolute hidden top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:block">
 				<ul class="navbar flex flex-col justify-center font-chivo gap-[34px] lg:flex-row">
 					<li class="flex items-center group">
 						<router-link
 							:to="{ name: 'index' }"
-							class="hover:text-green-900 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
+							class="hover:text-primary-0 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
 							<span>Inicio</span>
 						</router-link>
 					</li>
 					<li class="flex items-center group">
 						<router-link
 							:to="{ name: 'about' }"
-							class="hover:text-green-900 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
+							class="hover:text-primary-0 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
 							<span>Nosotros</span>
 						</router-link>
 					</li>
 					<li class="flex items-center group">
 						<router-link
 							:to="{ name: 'services' }"
-							class="hover:text-green-900 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
-							<span>Servicios</span>
+							class="hover:text-primary-0 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
+							<span> Servicios </span>
 						</router-link>
 					</li>
 					<li class="flex items-center group">
 						<router-link
 							:to="{ name: 'products' }"
-							class="hover:text-green-900 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
-							<span>Productos</span>
+							class="hover:text-primary-0 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
+							<span> Productos </span>
 						</router-link>
 					</li>
 					<li class="flex items-center group">
 						<router-link
 							:to="{ name: 'industries' }"
-							class="hover:text-green-900 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
-							<span>Industrias</span>
+							class="hover:text-primary-0 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
+							<span> Industrias </span>
 						</router-link>
 					</li>
 					<li class="flex items-center group">
 						<router-link
 							:to="{ name: 'contact' }"
-							class="hover:text-green-900 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
-							<span>Contacto</span>
+							class="hover:text-primary-0 text-base font-inter menu-link lg:text-heading-6 mr-[7px]">
+							<span> Contacto </span>
 						</router-link>
 					</li>
 				</ul>
 			</nav>
+			<!-- boton de iniciar Sesion -->
+			<div class="ml-auto">
+				<button type="button" class="ml-auto me-4">
+					<router-link
+						:to="{ name: 'contact' }"
+						class="m-4 me-4 flex items-center z-10 relative transition-all duration-200 group px-[10px] py-[12px] lg:px-[30px] lg:py-[15px] rounded-[50px] bg-primary-0 text-white hover:bg-gray-100 hover:text-gray-900 hover:-translate-y-[2px] text-heading-6 tracking-wide mr-[22px]">
+						<span class="block text-inherit w-full h-full rounded-[50px] text-lg font-chivo font-semibold">
+							Iniciar Sesion
+						</span>
+						<i class="group">
+							<img class="ml-[7px] w-[12px] fill:primary-0" src="C:\Users\ximem\OneDrive\Documentos\Ximena Medina\Pagina_CCEO_Software_Develop\cceo-web-page\src\assets\images\icon-right-white.svg" alt="Icono de flecha a la izquierda white" />
+						</i>
+					</router-link>
+				</button>
+			</div>
 		</div>
 
 		<div class="burger-icon burger-icon-white menu__icon block lg:hidden" @click="clickMenu">
@@ -59,7 +74,6 @@
 				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 			</svg>
 		</div>
-
 
 		<div v-if="open" class="fixed inset-0 bg-gray-900/80 z-0" />
 
@@ -76,43 +90,50 @@
 						<li class="group menu-mobile-item py-[13px]">
 							<router-link
 								:to="{ name: 'index' }"
-								class="flex items-center justify-between transition-all duration-200 hover:text-green-900 hover:translate-x-[2px]">
+								class="flex items-center justify-between transition-all duration-200 hover:text-primary-0 hover:translate-x-[2px]">
 								<span>Inicio</span>
 							</router-link>
 						</li>
 						<li class="group menu-mobile-item py-[13px]">
 							<router-link
 								:to="{ name: 'about' }"
-								class="flex items-center justify-between transition-all duration-200 hover:text-green-900 hover:translate-x-[2px]">
-								<span>Nosotros</span>
+								class="flex items-center justify-between transition-all duration-200 hover:text-primary-0 hover:translate-x-[2px]">
+								<span> Nosotros </span>
 							</router-link>
 						</li>
 						<li class="group menu-mobile-item py-[13px]">
 							<router-link
 								:to="{ name: 'services' }"
-								class="flex items-center justify-between transition-all duration-200 hover:text-green-900 hover:translate-x-[2px]">
-								<span>Servicios</span>
+								class="flex items-center justify-between transition-all duration-200 hover:text-primary-0 hover:translate-x-[2px]">
+								<span> Servicios </span>
 							</router-link>
 						</li>
 						<li class="group menu-mobile-item py-[13px]">
 							<router-link
 								:to="{ name: 'products' }"
-								class="flex items-center justify-between transition-all duration-200 hover:text-green-900 hover:translate-x-[2px]">
-								<span>Productos</span>
+								class="flex items-center justify-between transition-all duration-200 hover:text-primary-0 hover:translate-x-[2px]">
+								<span> Productos </span>
 							</router-link>
 						</li>
 						<li class="group menu-mobile-item py-[13px]">
 							<router-link
 								:to="{ name: 'industries' }"
-								class="flex items-center justify-between transition-all duration-200 hover:text-green-900 hover:translate-x-[2px]">
-								<span>Industrias</span>
+								class="flex items-center justify-between transition-all duration-200 hover:text-primary-0 hover:translate-x-[2px]">
+								<span> Industrias </span>
 							</router-link>
 						</li>
 						<li class="group menu-mobile-item py-[13px]">
 							<router-link
 								:to="{ name: 'contact' }"
-								class="flex items-center justify-between transition-all duration-200 hover:text-green-900 hover:translate-x-[2px]">
-								<span>Contacto</span>
+								class="flex items-center justify-between transition-all duration-200 hover:text-primary-0 hover:translate-x-[2px]">
+								<span> Contacto </span>
+							</router-link>
+						</li>
+						<li class="group menu-mobile-item py-[13px]">
+							<router-link
+								:to="{ name: 'contact' }"
+								class="flex items-center justify-between transition-all duration-200 hover:text-primary-0 hover:translate-x-[2px]">
+								<span> Iniciar Sesion </span>
 							</router-link>
 						</li>
 					</ul>
@@ -126,10 +147,34 @@
 </template>
 
 <script setup>
-    import { ref } from 'vue';
-    const open = ref(false);
+import { ref, onMounted, onUnmounted } from 'vue';
 
-    const clickMenu = () =>{
-        open.value = !open.value;
-    }
+const isScrolled = ref(false);
+const open = ref(false); //  menú móvil
+
+const clickMenu = () => { 
+  open.value = !open.value; 
+};
+
+const closeMenu = (event) => {
+  if (!event.target.closest('.nav-mobile') && !event.target.closest('.menu__icon')) {
+    open.value = false;
+  }
+};
+
+//Scroll de la parte del header.
+
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 50;
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+  document.addEventListener("click", closeMenu);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+  document.removeEventListener("click", closeMenu);
+});
 </script>
